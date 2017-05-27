c7b4713b13e71532f966b33d510f5bc1a87701c0
Vishal Anand <vishal_24_anand@hotmail.com>
2017-05-27 12:29:58 +0530

    Added area

---
+++ Voter/Areas/AdminModule/Controllers/AdminHomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Voter.Areas.AdminModule.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        
+        public ActionResult AdminHome()
+        {
+            return View();
+        }
+    }
+}
